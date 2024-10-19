0cd08c836073cb1903fb248e1e1922296bb5b895
Egerius <e.yanin@list.ru>
2024-10-19 23:22:56 +0300

    Add more tests for task №5

--- lab2/task5/tests/test.py
+++ lab2/task5/tests/test.py
@@ -1,9 +1,25 @@
 from lab2.task5.src.main import main
 import psutil
 import time
+import unittest
 
 
-start = time.perf_counter()
-main('input.txt')
-print(f'Затрачено времени: {round(time.perf_counter() - start, 6)} c.')
-print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
+class TestCaseTask5(unittest.TestCase):
+
+    def test_true(self):
+        start = time.perf_counter()
+        main('true.txt')
+        print(f'\nЗатрачено времени: {round(time.perf_counter() - start, 6)} c.')
+        print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
+        with open('output.txt') as f:
+            flag = bool(int(f.read().strip()))
+        self.assertTrue(flag)
+
+    def test_false(self):
+        start = time.perf_counter()
+        main('false.txt')
+        print(f'\nЗатрачено времени: {round(time.perf_counter() - start, 6)} c.')
+        print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
+        with open('output.txt') as f:
+            flag = bool(int(f.read().strip()))
+        self.assertFalse(flag)
