c07f590154e108801055104e2126273e1d80f2ea
Egerius <e.yanin@list.ru>
2024-10-19 19:28:11 +0300

    Add tests

--- lab2/task1/tests/test.py
+++ lab2/task1/tests/test.py
@@ -1,5 +1,9 @@
-from unittest import *
 from lab2.task1.src.main import main
+import psutil
+import time
 
 
+start = time.perf_counter()
 main('input.txt')
+print(f'Затрачено времени: {round(time.perf_counter() - start, 6)} c.')
+print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
