1f243001bce9e17f179f6f704fc2f255da81b6cd
Egerius <e.yanin@list.ru>
2024-11-13 13:16:22 +0300

    Refactor tests for task 3

--- lab3/task3/src/main.py
+++ lab3/task3/src/main.py
@@ -1,4 +1,10 @@
 from lab3.utils import is_sorted, read_array, write_vars, print_time_memory
+import os
+
+
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
+file_input = os.path.join(current_script_dir, '../textsf/input.txt')
+file_output = os.path.join(current_script_dir, '../textsf/output.txt')
 
 
 @print_time_memory
@@ -9,14 +15,14 @@ def pugalo_sort(lst, k):
 
 
 def task3():
-    data = read_array('../textsf/input.txt', num=2, with_len=False)
+    data = read_array(file_input, num=2, with_len=False)
     n, k = data[0]
     a = data[1]
     pugalo_sort(a, k)
     if is_sorted(a):
-        write_vars('../textsf/output.txt', 'YES')
+        write_vars(file_output, 'YES')
     else:
-        write_vars('../textsf/output.txt', 'NO')
+        write_vars(file_output, 'NO')
 
 
 if __name__ == '__main__':
--- lab3/task3/tests/test.py
+++ lab3/task3/tests/test.py
@@ -1,18 +1,25 @@
 import unittest
-from lab3.task3.src.main import pugalo_sort, task3
-from lab3.utils import read_array, is_sorted, write_vars, read_str_lines
+from lab3.task3.src.main import task3
+from lab3.utils import read_array, write_vars, read_str_lines
+import os
+
+
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
+file_input = os.path.join(current_script_dir, '../textsf/input.txt')
+file_output = os.path.join(current_script_dir, '../textsf/output.txt')
 
 
 class TestCase(unittest.TestCase):
 
     def test_should_task3(self):
-        data = read_array('../textsf/samples.txt', num=4, with_len=False)
+        path = os.path.join(current_script_dir, '../textsf/samples.txt')
+        data = read_array(path, num=4, with_len=False)
         expected_result = ['NO', 'YES']
 
         print()
         for i in range(2):
-            write_vars('../textsf/input.txt', data[i * 2], data[i * 2 + 1])
+            write_vars(file_input, data[i * 2], data[i * 2 + 1])
             task3()
-            ans = read_str_lines('../textsf/output.txt')[0].strip()
+            ans = read_str_lines(file_output)[0].strip()
 
             self.assertEqual(ans, expected_result[i])
