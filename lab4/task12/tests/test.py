725f3706d04b2b891fdb4516d80221543b518a32
Egerius <e.yanin@list.ru>
2024-12-01 20:34:32 +0300

    Add tests for leave and name methods

--- lab4/task12/tests/test.py
+++ lab4/task12/tests/test.py
@@ -18,10 +18,30 @@ class TestCase(unittest.TestCase):
         self.assertEqual(tmp.formation, expected_result)
 
     def test_should_leave(self):
-        pass
+        # given
+        tmp = Recruit()
+        tmp.stand_right(2, 1)
+        tmp.stand_right(3, 2)
+        expected_result = [1, 3]
+
+        # when
+        tmp.leave(2)
+
+        # then
+        self.assertEqual(tmp.formation, expected_result)
 
     def test_should_name(self):
-        pass
+        # given
+        tmp = Recruit()
+        tmp.stand_right(2, 1)
+        tmp.stand_right(3, 2)
+        expected_result = (1, 3)
+
+        # when
+        res = tmp.name(2)
+
+        # then
+        self.assertEqual(res, expected_result)
 
     def test_task12(self):
         pass
