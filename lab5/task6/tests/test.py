a65969c9c7df82f10ba5b747dee122dfe520cdaf
Egerius <e.yanin@list.ru>
2024-12-08 12:41:15 +0300

    Moved LowerHeap tests from task6 to task4

--- lab5/task6/tests/test.py
+++ lab5/task6/tests/test.py
@@ -3,31 +3,6 @@ import unittest
 
 
 class Lab5Task6TestCase(unittest.TestCase):
-
-    def test_should_max_heapify(self):
-        # given
-        lst = [34, 23, 45, 52, 13]
-        expected_result = [13, 23, 45, 52, 34]
-
-        # when
-        heap = LowerHeap(lst)
-        result = heap.get_list()
-
-        # then
-        self.assertEqual(result, expected_result)
-
-    def test_should_create_lower_heap(self):
-        # given
-        lst = [34, 52, 45, 23, 13]
-        expected_result = [13, 23, 45, 34, 52]
-
-        # when
-        heap = LowerHeap(lst)
-        result = heap.get_list()
-
-        # then
-        self.assertEqual(result, expected_result)
-
     def test_should_insert_queue(self):
         # given
         queue = LowerPriorityQueue()
