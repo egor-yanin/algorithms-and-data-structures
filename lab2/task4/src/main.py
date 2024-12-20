a1e155ff8c0cbb6dd538bc43cf643cb2ad0dfc48
Egerius <e.yanin@list.ru>
2024-12-20 12:43:11 +0300

    Refactor task4

--- lab2/task4/src/main.py
+++ lab2/task4/src/main.py
@@ -1,25 +1,37 @@
 from math import floor
 from lab2.utils import read_array, write_vars
+import os
 
 
-def binary_search(lst, low, high, key):
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_INPUT = os.path.join(CURRENT_DIR, '../txtf/input.txt')
+FILE_OUTPUT = os.path.join(CURRENT_DIR, '../txtf/output.txt')
+
+
+def binary_search(lst, key, low=0, high=None):
+    if high is None:
+        high = len(lst) - 1
     if high < low:
         return low - 1
-    if low == high:
-        return -1
     mid = low + floor((high - low) / 2)
     if key == lst[mid]:
         return mid
+    if high == mid == low:
+        return -1
     elif key < lst[mid]:
-        return binary_search(lst, low, mid - 1, key)
+        return binary_search(lst, key, low, mid - 1)
     else:
-        return binary_search(lst, mid + 1, high, key)
+        return binary_search(lst, key, mid + 1, high)
 
 
-def main(file='input.txt'):
-    input_data = read_array(file, num=2, with_len=True)
-    a, b = input_data[0][0], input_data[1][0]
+def task2():
+    input_data = read_array(FILE_INPUT, num=2, with_len=True)
+    lst, search_list = input_data[0][0], input_data[1][0]
     ans = []
-    for i in b:
-        ans.append(binary_search(a, 0, len(a), i))
-    write_vars('../tests/output.txt', ans)
+    for i in search_list:
+        ans.append(binary_search(lst, i))
+    write_vars(FILE_OUTPUT, ans)
+
+
+if __name__ == '__main__':
+    task2()
--- lab2/task4/tests/test.py
+++ lab2/task4/tests/test.py
@@ -1,9 +1,43 @@
-from lab2.task4.src.main import main
-import psutil
-import time
+from lab2.task4.src.main import binary_search, task2
+from lab2.utils import write_vars, read_array
+import unittest
+import os
 
 
-start = time.perf_counter()
-main('input.txt')
-print(f'Затрачено времени: {round(time.perf_counter() - start, 6)} c.')
-print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_INPUT = os.path.join(CURRENT_DIR, '../txtf/input.txt')
+FILE_OUTPUT = os.path.join(CURRENT_DIR, '../txtf/output.txt')
+FILE_TEST = os.path.join(CURRENT_DIR, '../txtf/test.txt')
+FILE_SAMPLE = os.path.join(CURRENT_DIR, '../txtf/sample.txt')
+
+
+class Lab2Task4TestCase(unittest.TestCase):
+    def test_should_binary_search(self):
+        # given
+        lst = [0, 3, 4, 6, 8]
+        expected_result = 3
+
+        # when
+        result = binary_search(lst, 6)
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_should_task2(self):
+        # given
+        data = read_array(FILE_TEST, num=2)
+        write_vars(FILE_INPUT, data[0][1], data[0][0], data[1][1], data[1][0])
+        expected_result = [3, 1, -1]
+
+        # when
+        task2()
+        result = read_array(FILE_OUTPUT, with_len=False)[0]
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    @classmethod
+    def tearDownClass(cls):
+        data = read_array(FILE_SAMPLE, num=2)
+        write_vars(FILE_INPUT, data[0][1], data[0][0], data[1][1], data[1][0])
+        pass
