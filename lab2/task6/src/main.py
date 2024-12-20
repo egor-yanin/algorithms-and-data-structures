49cbe54be1230a8d34da3c17994da89b58660ea7
Egerius <e.yanin@list.ru>
2024-12-20 14:47:19 +0300

    Refactor task6

--- lab2/task6/src/main.py
+++ lab2/task6/src/main.py
@@ -1,32 +1,40 @@
 from math import floor
+import os
 
 
-def find_max_crossing_subarray(A, low, mid, high):
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_INPUT = os.path.join(CURRENT_DIR, '../txtf/input.txt')
+FILE_OUTPUT = os.path.join(CURRENT_DIR, '../txtf/output.txt')
+
+
+def find_max_crossing_subarray(lst, low, mid, high):
     left_sum = -10**9
     s = 0
     for i in range(mid, low - 1, -1):
-        s += A[i]
+        s += lst[i]
         if s > left_sum:
             left_sum = s
             left_max = i
     right_sum = - 10**9
     s = 0
     for j in range(mid + 1, high + 1):
-        s += A[j]
+        s += lst[j]
         if s > right_sum:
             right_sum = s
             right_max = j
     return left_max, right_max, left_sum + right_sum
 
 
-def find_max_subarray(A, low, high):
+def find_max_subarray(lst, low=0, high=-10):
+    if high == -10:
+        high = len(lst) - 1
     if low == high:
-        return low, high, A[low]
+        return low, high, lst[low]
     else:
         mid = floor((low + high) / 2)
-        left_low, left_high, left_sum = find_max_subarray(A, low, mid)
-        right_low, right_high, right_sum = find_max_subarray(A, mid + 1, high)
-        cross_low, cross_high, cross_sum = find_max_crossing_subarray(A, low, mid, high)
+        left_low, left_high, left_sum = find_max_subarray(lst, low, mid)
+        right_low, right_high, right_sum = find_max_subarray(lst, mid + 1, high)
+        cross_low, cross_high, cross_sum = find_max_crossing_subarray(lst, low, mid, high)
         if left_sum >= right_sum and left_sum >= cross_sum:
             return left_low, left_high, left_sum
         elif right_sum >= left_sum and right_sum >= cross_sum:
@@ -35,9 +43,8 @@ def find_max_subarray(A, low, high):
             return cross_low, cross_high, cross_sum
 
 
-def main(file):
-    with open(file) as fin, open('../tests/output.txt', 'w', encoding='UTF-16') as fout:
-        headers = [x for x in fin.readline().split()]
+def task6():
+    with open(FILE_INPUT) as fin, open(FILE_OUTPUT, 'w', encoding='UTF-16') as fout:
         lst, date = [], []
         name = str()
         for s in fin.readlines():
@@ -46,9 +53,13 @@ def main(file):
             date.append(data[2])
             name = data[0]
         delta = [lst[i] - lst[i-1] for i in range(1, len(lst))]
-        start, end, stonks = find_max_subarray(delta, 0, len(delta)-1)
+        start, end, stonks = find_max_subarray(delta)
         fout.write(f'''{name}
 02.09.24 - 01.10.24
 Дата покупки: {date[start][:2]}.{date[start][2:4]}.{date[start][4:]}
 Дата продажи: {date[end][:2]}.{date[end][2:4]}.{date[end][4:]}
 Прибыль: {stonks}''')
+
+
+if __name__ == '__main__':
+    task6()
--- lab2/task6/tests/test.py
+++ lab2/task6/tests/test.py
@@ -1,9 +1,28 @@
-from lab2.task6.src.main import main
-import psutil
-import time
+from lab2.task6.src.main import task6, find_max_subarray
+from lab2.utils import read_stocks, write_vars, read_lines
+import unittest
+import os
 
 
-start = time.perf_counter()
-main('aeroflot.txt')
-print(f'Затрачено времени: {round(time.perf_counter() - start, 6)} c.')
-print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_INPUT = os.path.join(CURRENT_DIR, '../txtf/input.txt')
+FILE_OUTPUT = os.path.join(CURRENT_DIR, '../txtf/output.txt')
+FILE_AEROFLOT = os.path.join(CURRENT_DIR, '../txtf/aeroflot.txt')
+FIlE_SAMPLE = os.path.join(CURRENT_DIR, '../txtf/sample.txt')
+
+
+class Lab2Task6TestCase(unittest.TestCase):
+    def test_should_find_max_subarray(self):
+        # given
+        data = read_stocks(FILE_AEROFLOT)
+        expected_result = 0, 14, 10.25
+
+        # when
+        result = find_max_subarray(data)
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    @classmethod
+    def tearDownClass(cls):
+        write_vars(FILE_INPUT, *read_lines(FIlE_SAMPLE))
