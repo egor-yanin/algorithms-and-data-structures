3f9fe9ccb21c252ad8331510810c6213596fb529
Egerius <e.yanin@list.ru>
2024-12-21 13:59:46 +0300

    Add tests for task5

--- lab6/task5/tests/test.py
+++ lab6/task5/tests/test.py
@@ -1,5 +1,5 @@
 from lab6.utils import write_vars, read_lines
-from lab6.task2.src.main import execute
+from lab6.task5.src.main import count_votes
 import unittest
 import os
 
@@ -9,8 +9,17 @@ FILE_INPUT = os.path.join(CURRENT_DIR, '../txtf/input.txt')
 FILE_SAMPLE = os.path.join(CURRENT_DIR, '../txtf/sample.txt')
 
 
-class Lab6Task2TestCase(unittest.TestCase):
+class Lab6Task5TestCase(unittest.TestCase):
+    def test_should_count_votes(self):
+        # given
+        data = ['Ivanov 101', 'Batikov 987', 'Tsar 657', 'Ivanov 54', 'Petr 70', 'Batikov 11']
+        expected_result = {'Ivanov': 155, 'Batikov': 998, 'Tsar': 657, 'Petr': 70}
 
+        # when
+        result = count_votes(data)
+
+        # then
+        self.assertEqual(result, expected_result)
 
     @classmethod
     def tearDownClass(cls):
