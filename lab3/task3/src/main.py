ffcec1d8c85de0bf4b1db936790e770633bb0a21
Egerius <e.yanin@list.ru>
2024-12-21 10:46:51 +0300

    Refactor task3

--- lab3/task3/src/main.py
+++ lab3/task3/src/main.py
@@ -2,9 +2,9 @@ from lab3.utils import is_sorted, read_array, write_vars, print_time_memory
 import os
 
 
-current_script_dir = os.path.dirname(os.path.abspath(__file__))
-file_input = os.path.join(current_script_dir, '../txtf/input.txt')
-file_output = os.path.join(current_script_dir, '../txtf/output.txt')
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_INPUT = os.path.join(CURRENT_DIR, '../txtf/input.txt')
+FILE_OUTPUT = os.path.join(CURRENT_DIR, '../txtf/output.txt')
 
 
 @print_time_memory
@@ -15,14 +15,14 @@ def pugalo_sort(lst, k):
 
 
 def task3():
-    data = read_array(file_input, num=2, with_len=False)
+    data = read_array(FILE_INPUT, num=2, with_len=False)
     n, k = data[0]
-    a = data[1]
-    pugalo_sort(a, k)
-    if is_sorted(a):
-        write_vars(file_output, 'YES')
+    lst = data[1]
+    pugalo_sort(lst, k)
+    if is_sorted(lst):
+        write_vars(FILE_OUTPUT, 'YES')
     else:
-        write_vars(file_output, 'NO')
+        write_vars(FILE_OUTPUT, 'NO')
 
 
 if __name__ == '__main__':
--- lab3/task3/tests/test.py
+++ lab3/task3/tests/test.py
@@ -4,29 +4,29 @@ from lab3.utils import read_array, write_vars, read_str_lines
 import os
 
 
-current_script_dir = os.path.dirname(os.path.abspath(__file__))
-file_input = os.path.join(current_script_dir, '../txtf/input.txt')
-file_output = os.path.join(current_script_dir, '../txtf/output.txt')
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_INPUT = os.path.join(CURRENT_DIR, '../txtf/input.txt')
+FILE_OUTPUT = os.path.join(CURRENT_DIR, '../txtf/output.txt')
+FILE_TEST = os.path.join(CURRENT_DIR, '../txtf/test.txt')
+FILE_SAMPLE = os.path.join(CURRENT_DIR, '../txtf/sample.txt')
 
 
 class Lab3Task3TestCase(unittest.TestCase):
 
     def test_should_task3(self):
         # given
-        path = os.path.join(current_script_dir, '../txtf/test_data.txt')
-        data = read_array(path, num=2, with_len=False)
+        data = read_array(FILE_TEST, num=2, with_len=False)
         expected_result = 'YES'
 
         # when
-        write_vars(file_input, data[0], data[1])
+        write_vars(FILE_INPUT, data[0], data[1])
         task3()
-        ans = read_str_lines(file_output, num=1)[0].strip()
+        ans = read_str_lines(FILE_OUTPUT, num=1)[0].strip()
 
         # then
         self.assertEqual(ans, expected_result)
 
     @classmethod
     def tearDownClass(cls):
-        file_sample = os.path.join(current_script_dir, '../txtf/sample')
-        data_sample = read_array(file_sample, with_len=False, num=2)
-        write_vars(file_input, *data_sample)
+        data_sample = read_array(FILE_SAMPLE, with_len=False, num=2)
+        write_vars(FILE_INPUT, *data_sample)
