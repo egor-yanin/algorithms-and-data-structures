581e91c64e4716a44364f01fced9a23efd5a1a01
Egerius <e.yanin@list.ru>
2024-12-23 18:58:26 +0300

    Add lab2 in run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from lab2.run_tasks import run_task as lab2
 from lab3.run_tasks import run_task as lab3
 from lab4.run_tasks import run_task as lab4
 from lab5.run_tasks import run_task as lab5
@@ -7,7 +8,7 @@ from lab7.run_tasks import run_task as lab7
 
 TASK_LIST = {1: [1, 6, 9], 2: [1, 4, 5, 6, 8], 3: [1, 3, 5, 7], 4: [2, 4, 5, 12], 5: [1, 4, 6, 7], 6: [1, 2, 5, 6],
              7: [1, 2, 5, 6]}
-LAB_LIST = {3: lab3, 4: lab4, 5: lab5, 6: lab6, 7: lab7}
+LAB_LIST = {2: lab2, 3: lab3, 4: lab4, 5: lab5, 6: lab6, 7: lab7}
 
 
 def run_lab(lub_num):
